289e8e39090d68e8b39b8c5361806ec30b938e2f
Archana Potnis <potnis.archana@gmail.com>
2021-09-24 11:59:22 -0700

    Removed react-reveal/Fade

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Fade from "react-reveal/Fade";
 
 function Section({
   title,
@@ -11,19 +10,15 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <Fade bottom>
-        <ItemText>
-          <h1>{title}</h1>
-          <p>{description}</p>
-        </ItemText>
-      </Fade>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
       <Buttons>
-        <Fade bottom>
-          <ButtonGroup>
-            <LeftButton>{leftBtnText}</LeftButton>
-            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-          </ButtonGroup>
-        </Fade>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg"></DownArrow>
       </Buttons>
     </Wrap>
